e3528f3d1397c72e5d02d74349d6ab664fe0b8ef
Evan Vosh <wahotondo@gmail.com>
2024-07-20 06:48:14 +0200

    scroll update

--- src/App.vue
+++ src/App.vue
@@ -55,5 +55,20 @@ export default {
 </template>
 
 <style>
+::-webkit-scrollbar {
+  width: 0px;
 
+}
+
+::-webkit-scrollbar-track {
+  background: rgba(241, 241, 241, 0);
+}
+
+
+
+
+::-webkit-scrollbar-thumb:hover {
+  background: #020202;
+  border-radius: 6px;
+}
 </style>
